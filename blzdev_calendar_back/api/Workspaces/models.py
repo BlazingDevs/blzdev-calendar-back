7d33e612d868ba3b2e007d70ecb07f613f5667e2
kimsh1017 <kimsh8580@gmail.com>
2022-07-21 00:48:05 +0900

    create models

--- blzdev_calendar_back/api/Workspaces/models.py
+++ blzdev_calendar_back/api/Workspaces/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Workspace(models.Model):
-    workspace_name = models.CharField(max_length=100)
+    workspace_name = models.CharField(max_length=100) #workspace 이름
+
