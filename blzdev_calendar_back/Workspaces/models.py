dc37163019417fdf63b8f55cdb380864da05fda4
kimsh1017 <kimsh8580@gmail.com>
2022-07-21 01:38:50 +0900

    model migrate

--- blzdev_calendar_back/Workspaces/models.py
+++ blzdev_calendar_back/Workspaces/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Workspace(models.Model):
+class Workspaces(models.Model):
     workspace_name = models.CharField(max_length=100) #workspace 이름
 
--- blzdev_calendar_back/blzdev_calendar_back/settings.py
+++ blzdev_calendar_back/blzdev_calendar_back/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dev_log',
+    'Schedules',
+    'Workspaces',
 ]
 
 MIDDLEWARE = [
