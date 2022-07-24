427de6c2de053b10676d001aa0381738b0205523
Cho Sangwook <chosw1002@naver.com>
2022-07-24 23:37:47 +0900

    ➕  ADD: Separate settings.py

---
+++ blzdev_calendar_back/blzdev_calendar_back/secret.py
@@ -0,0 +1,13 @@
+SECRET_KEY = 'django-insecure-u=6^71z$$*y2(4y-eg@76u&2wavw%xktg=b@&4ctmuy=qnr^ev'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blzdev_db',
+        'USER': 'admin',
+        'PASSWORD': 'test1234',
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
