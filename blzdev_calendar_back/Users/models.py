05e88ef75df0dc0742f496f101ca57c884ac3644
Cho Sangwook <chosw1002@naver.com>
2022-07-31 23:33:58 +0900

    ✨ FEAT: Add user model, serializer, and views

--- blzdev_calendar_back/Users/models.py
+++ blzdev_calendar_back/Users/models.py
@@ -2,31 +2,24 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class Manager(BaseUserManager):
-    def create(self, user_id, user_password, user_name):
-        if(not user_id):
-            raise ValueError("id needed")
-        if(not user_password):
-            raise ValueError("password needed")
-        if(not user_name):
-            raise ValueError("name needed")
+    def create_user(self, user_id, user_name, password=None):
             
         user=self.model(
             user_id=user_id,
-            user_password=user_password
+            user_name=user_name,
         )
 
-        user.set_password(user_password)
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
 class User(AbstractBaseUser):
-    user_id=models.CharField(max_length=30)
-    user_password=models.CharField(max_length=30)
+    user_primary_id = models.AutoField(primary_key=True)
+    user_id=models.CharField(max_length=30, unique=True)
     user_name=models.CharField(max_length=100)
 
     objects = Manager()
 
-    USERNAME_FIELD = 'user_name'
-
+    USERNAME_FIELD = 'user_id'
     def __str__(self):
         return self.user_name
