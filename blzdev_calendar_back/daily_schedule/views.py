564d70b5065a8af00c109d4a2e736288995e33e4
kimsh1017 <kimsh8580@gmail.com>
2022-08-05 15:33:21 +0900

    ✨ FEAT: Create daily_schedule view

--- blzdev_calendar_back/daily_schedule/views.py
+++ blzdev_calendar_back/daily_schedule/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from rest_framework.decorators import api_view
+from Users.models import User
+from .serializers import ScheduleSerializer
+from Schedules.models import Schedules
 
 # Create your views here.
+@api_view(['GET'])
+def daily_schedule(request):
+    if request.method == 'GET':
+        if not 'date' in request.GET:
+            ResponseData = {"error_code" : 404, "error_message" : "there is no date in query", "data" : ""}
+        else:
+            date = request.GET['date']
+            ScheduleQuerySet = Schedules.objects.filter(start_date = date)
+            serializer = ScheduleSerializer(ScheduleQuerySet,many=True)
+            ResponseData = {"error_code" : 200, "error_message" : "", "data" : serializer.data}
+    return JsonResponse(ResponseData, status=200)
