d4e946f17d728c7e0147fac70ccde6133833b223
danhan <danhandev@gmail.com>
2022-08-08 11:48:36 +0900

    🐛 FIX: Split schedule api to fix api_view unfound error

--- blzdev_calendar_back/Schedules/urls.py
+++ blzdev_calendar_back/Schedules/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('schedules/<int:schedule_id>/', views.as_view(), name='schedule'),
+    path('<int:schedule_id>/', views.schedule_detail, name='schedule'),
+    # delete, put method 를 못 찾아서 일단 url를 나눠 get으로 받아옴
+    path('<int:schedule_id>/delete', views.delete_schedule_detail, name='schedule'),
+    path('<int:schedule_id>/put', views.put_schedule_detail, name='schedule'),
     # TODO: path 설정 /schedules?workspace_id={workspace_id}&&date={date}'
 ]
--- blzdev_calendar_back/Schedules/views.py
+++ blzdev_calendar_back/Schedules/views.py
@@ -1,26 +1,51 @@
-from django.shortcuts import render
-# from django.views import View
-from django.views.generic import DetailView
-from Schedules.models import *
-
-from Schedules.serializers import SchedulesSerializer
-# from accounts.models import Users
 from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
-from rest_framework import status
+from django.shortcuts import get_object_or_404
+from Schedules.models import *
+from Schedules.serializers import SchedulesSerializer
 
+
+@csrf_exempt
 # @api_view(['GET', 'PUT', 'DELETE'])
-# def patch_delete(request):
-#     if request.method == 'PATCH':
-#         user = get_object_or_404(models.User, user=request.user)
-#         # partial=True이므로 PATCH method가 가능함
-#         serializer = UserSerializer(user, data=request.data, partial=True)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#     elif request.method == 'DELETE':
-#         # auth의 User가 삭제되면 Model의 User 또한 삭제됨 (CASCADE)
-#         user = get_object_or_404(User, id=request.user.id)
-#         user.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+@api_view(['GET'])
+def schedule_detail(request, schedule_id=None):
+    schedule = get_object_or_404(Schedules, id=schedule_id)
+    response_data = {'error_code': -1, 'error_message': None, 'data': None}
+
+    if request.method == 'GET':
+        response_data['error_message'] = 'get schedule'
+        response_data['data'] = SchedulesSerializer(schedule).data
+
+    else:
+        response_data['error_code'] = 400
+        response_data['error_message'] = 'unvalid request method'
+
+    return Response(response_data)
+
+
+@api_view(['PUT'])
+def put_schedule_detail(request, schedule_id=None):
+    schedule = get_object_or_404(Schedules, id=schedule_id)
+    response_data = {'error_code': -1, 'error_message': None, 'data': None}
+
+    if request.method == 'PUT':
+
+        response_data['error_code'] = 200
+        response_data['error_message'] = 'put schedule'
+
+    return Response(response_data)
+
+
+@api_view(['DELETE'])
+def delete_schedule_detail(request, schedule_id=None):
+    schedule = get_object_or_404(Schedules, id=schedule_id)
+    response_data = {'error_code': -1, 'error_message': None, 'data': None}
+
+    if request.method == 'DELETE':
+        schedule.delete()
+
+        response_data['error_code'] = 200
+        response_data['error_message'] = 'Delete schedule'
+
+    return Response(response_data)
