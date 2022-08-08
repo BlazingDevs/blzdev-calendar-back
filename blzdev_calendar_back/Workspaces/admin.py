464d13bb9b30cec6f41a66a4c52250247cced382
danhan <danhandev@gmail.com>
2022-08-08 11:46:12 +0900

    ➕ ADD: Register workspaces, user model in admin site

--- blzdev_calendar_back/Workspaces/admin.py
+++ blzdev_calendar_back/Workspaces/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Workspaces, UserWorkspaces
 
-# Register your models here.
+admin.site.register(Workspaces)
+admin.site.register(UserWorkspaces)
