ffe9be96a49b94ab42a50f43920fd490c94c2b7e
danhan <danhandev@gmail.com>
2022-08-08 11:45:16 +0900

    ➕ ADD: FIx schedule serializer to exclude users field

--- blzdev_calendar_back/Schedules/serializers.py
+++ blzdev_calendar_back/Schedules/serializers.py
@@ -4,6 +4,21 @@ from Users.models import User
 
 
 class SchedulesSerializer(serializers.ModelSerializer):
+    # count_members = users.objects.annotate(dep_count=Count('employee')).count()
     class Meta:
         model = Schedules
-        fields = '__all__'
+        exclude = ('users', )
+        # fields = '__all__'
+
+# class ScheduleMemberSerializer(serializers.ModelSerializer):
+#     def get_members(self,instance):
+#         result = dict()
+    
+#         user_schedule = UserWorkspaces.objects.filter(workspace__id=instance.id).values_list('user__user_primary_id',flat=True)
+#         result['count'] = users.count()
+#         result['member'] = WorkspaceUserSerializer(User.objects.filter(user_primary_id__in=user_schedule),many=True).data
+        
+#         return result
+    
+#     schedule_id = serializers.ReadOnlyField(source='id')
+#     members = serializers.SerializerMethodField()
