bd9baa9260085acfb1d7013741d16d71db41eecd
danhan <danhandev@gmail.com>
2022-08-09 12:06:06 +0900

    ♻️ REFACTOR: Change Schedule views to CBV

--- blzdev_calendar_back/Schedules/urls.py
+++ blzdev_calendar_back/Schedules/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:schedule_id>/', views.schedule_detail, name='schedule'),
+    path('<int:id>/', views.ScheduleDetailAPI.as_view()),
     path('', views.ScheduleListAPI.as_view()),
-    # TODO: path 설정 /schedules?workspace_id={workspace_id}&&date={date}'
 ]
