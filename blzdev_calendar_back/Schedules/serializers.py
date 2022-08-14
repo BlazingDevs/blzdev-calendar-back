42d24cbaa15528ba93c2b5c2b65797adf9b9b50a
danhan <danhandev@gmail.com>
2022-08-14 23:19:43 +0900

    ♻️ REFACTOR: Add custom error message in schedules

--- blzdev_calendar_back/Schedules/serializers.py
+++ blzdev_calendar_back/Schedules/serializers.py
@@ -1,6 +1,8 @@
+from asyncio.windows_events import NULL
 from rest_framework import serializers
 from Schedules.models import Schedules
 from Users.models import User
+
 
 class SchedulesSerializer(serializers.ModelSerializer):
 
@@ -8,3 +10,38 @@ class SchedulesSerializer(serializers.ModelSerializer):
         model = Schedules
         fields = ('id', 'schedule_name', 'workspace_id',
                   'date', 'time', 'members_id')
+
+
+class ScheduleMemberSerializer(serializers.ModelSerializer):
+    user_id = serializers.ReadOnlyField(source='user_primary_id')
+
+    class Meta:
+        model = User
+        fields = ('user_id', 'user_name')
+
+
+class SchedulesGetSerializer(serializers.ModelSerializer):
+    # Schedules 모델에 없는 새로운 members field를 추가함.
+    members = serializers.SerializerMethodField()
+
+    def get_members(self, obj):
+        # UserQuerySet = User.objects.values_list(
+        #     'obj__members_id', flat=True)
+        serializer = ScheduleMemberSerializer(obj, many=True)
+
+        # members = {"count": 0, "member": serializer.data}
+        members = {"count": 0, "member": NULL}
+        print(obj)
+        return members
+
+    class Meta:
+        model = Schedules
+        fields = ('id', 'schedule_name', 'workspace_id',
+                  'date', 'time', 'members')
+
+class SchedulesListSerializer(serializers.ModelSerializer):
+    schedule_id = serializers.ReadOnlyField(source='id')
+
+    class Meta:
+        model = Schedules
+        fields = ('workspace_id', 'schedule_id', 'schedule_name', 'date')
--- blzdev_calendar_back/Schedules/urls.py
+++ blzdev_calendar_back/Schedules/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id>/', views.ScheduleDetailAPI.as_view()),
-    path('', views.ScheduleListAPI.as_view()),
+    path('<int:schedule_id>/', views.ScheduleDetailAPI),
+    path('', views.ScheduleListAPI),
 ]
